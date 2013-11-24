93abc54d46108b9c9c97c051fb6c95618e537652
Phil <philip.dasler@gmail.com>
2013-11-24 09:48:49 -0500

    Fleshed out vehicle and reorganized file structure.

---
+++ Frogger/src/model/Vehicle.java
@@ -0,0 +1,51 @@
+package model;
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+
+
+public class Vehicle {
+	private float length;
+	private ArrayList<Float> location;
+	
+	private float speed;
+	private ArrayList<Float> direction;
+	
+	
+
+	Vehicle() {
+		length = 0;
+		location = null;
+		
+		speed = 0;
+		direction = null;
+	}
+	
+	Vehicle(float length, ArrayList<Float> location, float speed, ArrayList<Float> direction) throws InvalidParameterException{
+		if( location.size() != direction.size() )
+			throw new InvalidParameterException( "Dimensionality of direction and location do not match." );
+		
+		float dir_sum = 0;
+		for(int index=0; index < direction.size(); index++)
+			dir_sum += direction.get(index);
+		
+		if( dir_sum == 0)
+			throw new InvalidParameterException( "Direction vector equals 0.");
+		
+		// Normalize direction of travel
+		for(int index = 0; index < direction.size(); index++)
+			direction.set(index, direction.get(index)/dir_sum);
+		
+		this.length = length;
+		this.location = location;
+		
+		this.speed = speed;
+		this.direction = direction;
+	}
+	
+	void updatePosition(float timestep) {
+		for(int index = 0; index < location.size(); index++)
+			location.set(index, location.get(index) + direction.get(index) * speed * timestep);
+	}
+}
+
+
